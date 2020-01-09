7d50ca6850f868637d191acc91da93dc2ca494ba
Joey Romo <joeyjr95@gmail.com>
2020-01-09 10:58:52 -0500

    context/future proofing"

--- src/App/App.js
+++ src/App/App.js
@@ -4,6 +4,7 @@ import LandingPage from "../Routes/LandingPage/LandingPage";
 import Footer from '../Routes/Footer/Footer';
 import { Route, Switch } from "react-router-dom";
 
+
 export default class App extends React.Component {
   render() {
     return (
